e3f10fd782ed4fa2aaa9e570f91e817e994d557e
Tanmay Kathane <TanmayKathane68@gmail.com>
2025-11-22 01:33:36 +0530

    Remove server start code
    Removed server start code from backend/server.js.

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,4 @@ app.use("/api/auth", authRoutes);
 app.use("/api/stores", storeRoutes);
 app.use("/api", reviewRoutes);
 
-// start
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
